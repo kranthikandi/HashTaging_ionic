28ced065929c4e74bf01b608f62beebbdb2a35d3
kranthi kandi <kranthikumar.kandi@gmail.com>
2016-04-16 14:51:30 -0700

    new css, added tweets and profile images, some code for media

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,6 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,$http) {
+    $scope.getData = function(){
+  var url = "https://search-proxy.spredfast.com/search.json?q=junglebook&filter.start=-1d&filter.finish=0&view.entities.limit=500";
+  $http.get(url).success(function(data){
+    var data = data.views.entities.data;
+    $scope.datas = [];
+    var listdata = [];
+    var media = [];
+    for (var i = 0; i < data.length; i++) {
+      listdata.push({
+        tweet : data[i].raw.text,
+        imgUrl : data[i].raw.user.profile_image_url,
+        });
+         media.push = data[i].raw.extended_entities
+    }
+    $scope.datas = listdata;
+    $scope.media = media;
+    console.log(data);
+    console.log($scope.media);
+
+       
+       //$scope.profileImgUrl = $scope.datas.raw.user.profile_image_url;
+      
+  });
+}
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
